7f591c94a2497d23d65d9060e64ebd41298af96b
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-03-01 17:43:03 +0530

    Removed

--- src/video.service.js
+++
@@ -1,8 +0,0 @@
-export const videoService = {
-    getLink(keyword){
-        let promise = fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${keyword}&key=AIzaSyDGqsUprWi3Rol5FqKb1fNDX-zRyGfgZMc`)
-        .then(res => res.json())
-        .catch(err => Promise.reject(err));
-        return promise;
-    }
-}
