5b8f89004415ce590e42ceaef95538520d8b1f7f
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-03-02 23:32:53 +0530

    Use json to provide input for User object creation

--- Backend/data_registry/__init__.py
+++ Backend/data_registry/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 
 from mongoengine import connect
 from models import User
 import urllib
+import json
 import ssl
 
 PASSWORD = "Musiqplayer@123"
@@ -17,16 +18,7 @@ api = Api(app)
 
 class CreateUser(Resource):
     def post(self):
-        parser = reqparse.RequestParser(bundle_errors=True)
-        parser.add_argument("firstName", required=True)
-        parser.add_argument("lastName", required=True)
-        parser.add_argument("email", required=True)
-        parser.add_argument("phone", required=True)
-        parser.add_argument("password", required=True)
-        parser.add_argument("country")
-        parser.add_argument("state")
-        parser.add_argument("city")
-        user = parser.parse_args()
+        user = json.dumps(request.get_json())
         connect('YY-MP-DB', host=HOST_NAME, ssl_cert_reqs=ssl.CERT_NONE)
         newUser = User.from_json(json_data=user)
         newUser.save()
