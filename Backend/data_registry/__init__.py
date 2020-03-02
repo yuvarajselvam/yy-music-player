761bd004770e32e271fb9faac443b8d83243e6d7
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-03-03 00:08:02 +0530

    Use json to provide input for User object creation

--- Backend/data_registry/__init__.py
+++ Backend/data_registry/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+import sys
+
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_restful import Resource, Api
 
@@ -9,7 +11,8 @@ import json
 import ssl
 
 PASSWORD = "Musiqplayer@123"
-HOST_NAME = "mongodb+srv://yympserver:" + urllib.parse.quote_plus(PASSWORD) + "@yy-music-player-dev-fygew.mongodb.net/YY-MP-DB?retryWrites=true&w=majority"
+HOST_NAME = "mongodb+srv://yympserver:" + urllib.parse.quote_plus(
+    PASSWORD) + "@yy-music-player-dev-fygew.mongodb.net/YY-MP-DB?retryWrites=true&w=majority"
 
 app = Flask(__name__)
 CORS(app)
@@ -18,10 +21,15 @@ api = Api(app)
 
 class CreateUser(Resource):
     def post(self):
+        print(request.get_json())
         user = json.dumps(request.get_json())
         connect('YY-MP-DB', host=HOST_NAME, ssl_cert_reqs=ssl.CERT_NONE)
-        newUser = User.from_json(json_data=user)
-        newUser.save()
+        try:
+            newUser = User.from_json(json_data=user)
+            newUser.save()
+        except:
+            return jsonify(Error=str(sys.exc_info()[1]))
+
         print(newUser.id)
         return newUser.to_json(), 201
 
