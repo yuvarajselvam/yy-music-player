82344b9cc54f4f93644983b5b6a82ced4a83d10c
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-04-24 23:00:23 +0530

    Playlist functionalities

---
+++ Backend/models/PlaylistModel.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from mongoengine import DynamicDocument, DynamicEmbeddedDocument, EmbeddedDocumentListField, \
+    StringField, DateTimeField, LongField, URLField, IntField, LazyReferenceField, ValidationError
+
+
+class PlaylistTrack(DynamicEmbeddedDocument):
+    id = StringField()
+    name = StringField(required=True)
+    type = StringField(required=True, choices=["local", "library"])
+    path = URLField()
+    language = StringField()
+    artists = StringField()
+    # TODO: Fill in choices with only supported languages here
+
+    def clean(self):
+        if self.type == "library" and not self.id:
+            raise ValidationError("Track ID is required for type:library tracks.")
+            # TODO: Verify Track ID
+
+
+class Playlist(DynamicDocument):
+    meta = {'collection': 'playlists'}
+    name = StringField(required=True, min_length=1)
+    type = StringField(required=True, default="playlist")
+    scope = StringField(default="private", choices=["public", "private", "system"])
+    owner = StringField()
+    tracks = EmbeddedDocumentListField(PlaylistTrack)
+    totalTracks = IntField(default=0, min_value=0)
+    imageUrl = URLField()
+    addedByCount = LongField(default=1, min_value=0)
+    createdAt = DateTimeField(default=datetime.now)
+
+    def clean(self):
+        if self.scope != "system" and not self.owner:
+            raise ValidationError("Owner is required for user created playlists.")
+
+        # TODO: Verify unique Track ID
