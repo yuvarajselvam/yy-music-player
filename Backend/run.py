5de5d29d4567d3fd1fbf34466685547f7a8dd0da
Yuvaraj Selvam <imyuvarajselvam@gmail.com>
2020-05-15 22:43:22 +0530

    Friend requests and logging

--- Backend/run.py
+++ Backend/run.py
@@ -1,18 +1,24 @@
 import os
+import time
 
 from api.search.album import GetAlbum
 from api.search.artist import GetArtist
 from api.search.autocomplete import Autocomplete
+from api.search.saavn_track import GetTrack
 from api.auth.authenticate import Authenticate
 from api.auth.change_password import ChangePassword
 from api.auth.create_user import CreateUser
 from api.auth.forgot_password import ForgotPassword, ValidatePasswordChangeToken
 from api.auth.single_sign_on import SingleSignOn
-from api.search.saavn_track import GetTrack
-from api.search.playlist import GetPlaylist, CreatePlaylist, DeletePlaylist, EditPlaylist, ListPlaylist
+from api.auth.register_device import RegisterDevice
+from api.playlist.playlist import GetPlaylist, CreatePlaylist, DeletePlaylist, EditPlaylist, ListPlaylist
+from api.social.follow import ListFollowers, ListFollowing, FollowUser, RespondToFollowRequest
+from api.social.user_search import UserSearch, GetUser, GetUserByEmail
 
+from utils.notifications import NotificationUtil
 from utils.secrets import Secrets
 from utils.db import DbUtils
+from utils.logging import Logger
 
 from flask import Flask
 from flask_cors import CORS
@@ -20,14 +26,15 @@ from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 os.environ.__setitem__('verbose', 'abs')
-if os.environ['verbose']:
-    print("[INFO] Running in verbose mode. \n")
 
+logger = Logger("run").logger
+start_time = time.time()
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = Secrets.JWT_SECRET_KEY
 CORS(app)
 api = Api(app)
 jwt = JWTManager(app)
+NotificationUtil().init_firebase()
 
 
 @app.route('/')
@@ -38,9 +45,9 @@ def StartupPage():
 api.add_resource(CreateUser, '/signup/')
 api.add_resource(Authenticate, '/signin/')
 api.add_resource(SingleSignOn, '/sso/')
-api.add_resource(ForgotPassword, '/forgot_password/')
-api.add_resource(ValidatePasswordChangeToken, '/forgot_password/validate/')
-api.add_resource(ChangePassword, '/change_password/')
+api.add_resource(ForgotPassword, '/forgot-password/')
+api.add_resource(ValidatePasswordChangeToken, '/forgot-password/validate/')
+api.add_resource(ChangePassword, '/change-password/')
 api.add_resource(GetTrack, '/track/<_language>/<_id>/')
 api.add_resource(GetAlbum, '/album/<_language>/<_id>/')
 api.add_resource(GetArtist, '/artist/<_id>/')
@@ -50,11 +57,19 @@ api.add_resource(ListPlaylist, '/user/<_user_id>/playlists/')
 api.add_resource(CreatePlaylist, '/playlist/create/')
 api.add_resource(EditPlaylist, '/playlist/<_id>/edit/')
 api.add_resource(DeletePlaylist, '/playlist/<_id>/delete/')
+api.add_resource(ListFollowers, '/user/<_user_id>/followers/')
+api.add_resource(ListFollowing, '/user/<_user_id>/following/')
+api.add_resource(UserSearch, '/users/')
+api.add_resource(GetUser, '/user/<_user_id>/')
+api.add_resource(GetUserByEmail, '/user/email/<_user_email>/')
+api.add_resource(FollowUser, '/follow-request/')
+api.add_resource(RespondToFollowRequest, '/follow-request/<_op>/')
+api.add_resource(RegisterDevice, '/register-device/')
 
+DbUtils().setup_schema()
 DbUtils().db_connect()
 
 port = int(os.environ.get("PORT", 5000))
-app.run('0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=port)
 
 DbUtils().db_disconnect()
-# host="0.0.0.0"
--- Backend/utils/db.py
+++ Backend/utils/db.py
@@ -1,16 +1,14 @@
-from mongoengine import connect, disconnect
+from mongoengine import connect, disconnect, PULL, CASCADE
 import ssl
 from pymongo import MongoClient
 from utils.secrets import Secrets
 
+from models.PlaylistModel import Playlist
+from models.UserModel import User, PendingRequest
 
 client = MongoClient()
 Saavn = client.YYMPSaavn2
-tracks = Saavn.tracks_tamil
-albums = Saavn.albums_tamil
 artists = Saavn.artists
-albums_telugu = Saavn.albums_telugu
-tracks_telugu = Saavn.tracks_telugu
 
 
 class DbUtils:
@@ -19,7 +17,16 @@ class DbUtils:
 
     def db_connect(self):
         self.db_alias = connect('YY-MP-DB', host=self.host, ssl_cert_reqs=ssl.CERT_NONE)
+        print("Db connected")
 
     def db_disconnect(self):
         disconnect(self.db_alias)
 
+    @staticmethod
+    def setup_schema():
+        User.register_delete_rule(Playlist, 'playlists', PULL)
+        User.register_delete_rule(PendingRequest, 'pendingRequests', PULL)
+        Playlist.register_delete_rule(User, 'owner', CASCADE)
+
+    def get_pymongo_connection(self):
+        return MongoClient(self.host)
